75f5746d1c90192d41a677784a84b37a2c1ce0e0
Sunil579 <Sunil.giri9803@gmail.com>
2023-12-10 09:42:49 +0545

    Update SubjectController.cs

--- Tutorial 5 EX 2/Controllers/SubjectController.cs
+++ Tutorial 5 EX 2/Controllers/SubjectController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,29 +8,53 @@ namespace Tutorial_5_EX_2.Controllers
 {
     public class SubjectController : Controller
     {
+        // Dummy class to represent subjects
+        public class Subject
+        {
+            public int SubjectID { get; set; }
+            public string SubjectName { get; set; }
+        }
+
         // GET: Subject
-        public String Index()
+        public ActionResult Index()
         {
-            return "this page is to show subject home page";
+            return View();
         }
-        public String subjects()
-        {
-            return "This page is to show subjects that the student is enrolled in ";
-        }
-        public string Detail(string SubjectID)
-        {
 
-            return HttpUtility.HtmlEncode("Subjectcode ;" + SubjectID);
+        // GET: Subjects
+        public ActionResult Subjects()
+        {
+            
+            var subjects = new List<Subject>
+            {
+                new Subject { SubjectID = 1, SubjectName = "Mathematics" },
+                new Subject { SubjectID = 2, SubjectName = "Science" },
+                new Subject { SubjectID = 3, SubjectName = "History" }
+            };
 
+            return View(subjects); // Pass the list to the view
         }
+
+        // GET: Subject/Detail/5
+        public ActionResult Detail(int SubjectID)
+        {
+           
+            var subject = new Subject { SubjectID = SubjectID, SubjectName = "Sample Subject" };
+            return View(subject); 
+        }
+
+        // GET: Subject/Edit/5
         public ActionResult Edit(int subjectID)
         {
-            return RedirectToAction("Index");
-        }
-        public ActionResult Deleted(int SubjectID)
-        {
-            return RedirectToAction("Index");
+           
+            return View(); // Return a view to edit the subject
         }
 
+        // GET: Subject/Deleted/5
+        public ActionResult Deleted(int SubjectID)
+        {
+            
+            return RedirectToAction("Index"); // Redirect to the Index view after deletion
+        }
     }
 }
